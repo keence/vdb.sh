e90d1e81cd9e7db7c07468dc1647a319cbbf2e08
keence <q907812@hotmail.com>
2019-04-21 23:00:58 +0800

    封面下载测试，不可使用，可能会改为单独可用

--- pdb_test.sh
+++ pdb_test.sh
@@ -1,17 +1,17 @@
 #！/bin/bash
 #author：九月(keence)
 
-str=$(curl "https://search.bilibili.com/all?keyword=av49563991")
+str=$(curl 'https://www.bilibili.com/video/av44689307?from=search&seid=7924668100199429519')
 #echo "$str" > /tmp/tmp2
 tmp1=${str#*'","description"'}
 #echo "$tmp1" > /tmp/tmp1
 tmp2=${str%'u002Farchive\u002F'*}
 str=${str%"$tmp1"}
 str=${str#"$tmp2"}
-echo "$str" > /tmp/tmp
+#echo "$str" > /tmp/tmp
 str=${str##*u002F}
 str=${str%%'"'*}
 echo $str
-cd /tmp
+#cd /tmp
 #wget https://i2.hdslb.com/bfs/archive/$str
 #echo $?
